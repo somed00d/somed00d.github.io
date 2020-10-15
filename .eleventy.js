9fea75874be7dbd450cfe1f63aed44daf953d798
somed00d <41293874+somed00d@users.noreply.github.com>
2020-10-15 12:22:44 -0400

    Initial set up and first post

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,44 @@
 const day = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
 
 day.locale('en');
+day.extend(relativeTime);
 
 module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addFilter('dateISO', date => {
-    return day(date);
+  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+  eleventyConfig.addFilter('dateFrom', date => {
+    return day(date).fromNow();
   });
 
   eleventyConfig.addFilter('dateReadable', date => {
-    return day(date).format('hh:mm:ss A, MM-DD-YYYY');
+    return day(date).format('h:mm:ss A - MM/DD/YYYY');
   });
 };
+
+function extractExcerpt(article) {
+  if (!article.hasOwnProperty('templateContent')) {
+    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+    return null;
+  }
+ 
+  let excerpt = null;
+  const content = article.templateContent;
+ 
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+    { start: '<p>', end: '</p>' }
+  ];
+ 
+  separatorsList.some(separators => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+ 
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+      return true; // Exit out of array loop on first match
+    }
+  });
+ 
+  return excerpt;
+}
--- posts/2020/post-2.md
+++
@@ -1,8 +0,0 @@
----
-layout: base.njk
-title: Building this website
-date: 2020-10-15 11:00:00 -4
-tags: ['general', 'development']
----
-
-Hello
